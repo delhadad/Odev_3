65132af1c382d832a99564506a09578b408b80c2
Duaa <112613757+delhadad@users.noreply.github.com>
2023-12-24 22:04:50 +0300

    Add files via upload

---
+++ Cuckoo.java
@@ -0,0 +1,21 @@
+public class Cuckoo extends AbstractCuckoo{
+    /**
+     * Yapıcı metodu ve imzasını değiştirmeyin, aksi halde testleriniz çalışmaz
+     */
+    public Cuckoo(int max_rehash_attempts, HashTable table1, HashTable table2) {
+        super(max_rehash_attempts, table1, table2);
+    }
+
+    @Override
+    public boolean insert(int key) {
+        // Metodu yazınız
+        return false;
+    }
+
+    @Override
+    public boolean search(int key) {
+        // Metodu yazınız
+        return false;
+    }
+
+}
